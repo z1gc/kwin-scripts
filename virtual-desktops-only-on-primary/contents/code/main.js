ae1bae10f0acb9dda330b7412f174bddd452c8f2
Zigit Zo <hardirq@outlook.com>
2024-08-27 13:49:39 +0800

    use display name instead of reference, which might be changed after disconnect

--- virtual-desktops-only-on-primary/contents/code/main.js
+++ virtual-desktops-only-on-primary/contents/code/main.js
@@ -1,6 +1,6 @@
 const screensNum = workspace.screens.length;
 const desktopsNum = workspace.desktops.length;
-const primaryScreen = workspace.activeScreen;
+const primaryScreen = workspace.activeScreen.name;
 
 function bind(window) {
     window.previousScreen = window.screen;
@@ -21,16 +21,16 @@ function update(window) {
         return;
 
     const onDesktops = window.onAllDesktops;
-    const currentScreen = window.output;
+    const currentScreen = window.output.name;
     const previousScreen = window.previousScreen;
     window.previousScreen = currentScreen;
 
-    if (currentScreen != primaryScreen) {
+    if (currentScreen !== primaryScreen) {
         if (!onDesktops) {
             window.onAllDesktops = true;
             print("Window " + window.caption + " has been pinned");
         }
-    } else if (previousScreen != primaryScreen) {
+    } else if (previousScreen !== primaryScreen) {
         if (onDesktops) {
             window.desktops = [workspace.currentDesktop];
             print("Window " + window.caption + " has been unpinned");
@@ -45,8 +45,7 @@ function bindUpdate(window) {
 
 function main() {
     // https://github.com/wsdfhjxc/kwin-scripts/issues/9#issuecomment-1744811107
-    print(`total screens: ${screensNum}, desktops: ${desktopsNum}`);
-    print(`primary output: ${primaryScreen.name} ${primaryScreen.manufacturer} ${primaryScreen.model} ${primaryScreen.serialNumber}`);
+    print(`total screens: ${screensNum}, desktops: ${desktopsNum}, primary: ${primaryScreen}`);
 
     // global object exposed via: KWin::Script::slotScriptLoadedFromFile
     workspace.windowList().forEach(bind);
